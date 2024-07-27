ea06a4d5a1b6a0c5fd29f15138964742ef3e673a
PK <89706786+wfkpk@users.noreply.github.com>
2024-07-27 20:24:38 +0530

    firebase added

---
+++ src/firebase/firebase-auth.guard.ts
@@ -0,0 +1,69 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { FirebaseAuthStrategy } from './firebase-auth.strategy';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class FirebaseAuthGuard extends AuthGuard('firebase-auth') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>('public', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+    console.log('FirebaseAuthGuard');
+    return super.canActivate(context);
+  }
+}
+
+@Injectable()
+export class UserAuthGuard {
+  constructor(
+    private readonly firebaseAuth: FirebaseAuthStrategy,
+    private prisma: PrismaService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const authHeader = request.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return false;
+    }
+
+    const firebaseUser = request.user;
+
+    if (!firebaseUser) {
+      return false;
+    }
+    const firebaseUid: string = firebaseUser.uid;
+    const user = await this.prisma.user.findUnique({
+      where: {
+        firebaseUid: firebaseUid,
+      },
+    });
+
+    if (!user) {
+      return false;
+    }
+
+    request.headers['userId'] = user.id;
+
+    return true;
+  }
+}
+
+@Injectable()
+export class NotAuthGuard {
+  canActivate(): boolean {
+    return true;
+  }
+}
